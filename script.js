02093645e8e22ef755f64dd89b8cd05547317946
patr612e <44566563+patr612e@users.noreply.github.com>
2020-04-29 11:13:26 +0200

    cdn-scrollmagic

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-import { gsap } from "gsap";
+let tl = new TimelineMax();
 
-gsap.from(".stel-row", { x: -400, duration: 2.5, ease: "back", stagger: 0.2 });
+const controller = new ScrollMagic.Controller();
+
+tl.from(".animate-me", 1, { x: window.innerWidth });
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".animate-me",
+  triggerHook: 1, // "onLeave", 0-1
+  duration: "100%"
+})
+  // .setPin(".canvas")
+  .setTween(tl)
+  .addTo(controller)
+  .addIndicators();
+
+//LORT NUMMER 2
+
+let tl2 = new TimelineMax();
+
+const controller2 = new ScrollMagic.Controller();
+
+tl2.to(".animate-me-2", 1, { x: window.innerWidth });
+
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: ".animate-me-2",
+  triggerHook: 1,
+  duration: "100%"
+})
+  .setTween(tl2)
+  .addTo(controller2);
